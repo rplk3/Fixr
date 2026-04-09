3e34685d10e7a40dc68ac0e88c29b0813c5e6a77
Ranuda Perera <ranudaperera@gmail.com>
2026-04-10 01:32:13 +0530

    implement user registration with password hashing and validation

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const connectDB = require("./config/db");
+
 const app = express();
 
+// connect DB
+connectDB();
+
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Fixr Backend Running 🚀");
+  res.send("Fixr API Running 🚀");
 });
 
 const PORT = 5000;
