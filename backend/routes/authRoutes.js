926576c67430a3b7786721614542252bfed5d52f
Ranuda Perera <ranudaperera@gmail.com>
2026-04-13 03:26:14 +0530

    add: a protected test route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,8 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const { registerUser, loginUser } = require("../controllers/authController");
-
+const protect = require("../middleware/authMiddleware");
+    
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/profile", protect, (req, res) => {
+  res.status(200).json({ 
+    MessageChannel: "User profile accessed successfully",
+    user: req.user });
+});
 
 module.exports = router;
