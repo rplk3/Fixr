3414577d35f9eef2ffbf8555a69daa345c16c233
IT23320482 <it23320482@my.sliit.lk>
2026-04-13 17:37:01 +0530

    add: implement route for retrieving a single service by ID

--- backend/routes/serviceRoutes.js
+++ backend/routes/serviceRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getServices, createService } = require("../controllers/serviceController");
+const { getServices, createService, getServiceById } = require("../controllers/serviceController");
 const protect = require("../middleware/authMiddleware");
 
 router.get("/", getServices);
 router.post("/", protect, createService);
+router.get("/:id", protect, getServiceById);
 
 module.exports = router;
 
